15cf3af27df47d5bba3e8dca6261536e9c55f795
otomototomoni <tegurechafu150415@icloud.com>
2025-05-18 14:40:28 +0900

    バージョンを3.2.5に変更 こちらの方が安定しているらしい。

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("jvm") version "2.1.0"
 	kotlin("plugin.spring") version "2.1.0"
-	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "2.1.0"
 }
